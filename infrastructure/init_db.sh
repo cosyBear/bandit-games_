862978ef1cd7f827168c2fd18e1a2b90b9009139
Ali Al-Zamili <ali.alzamili@student.kdg.be>
2024-11-26 12:54:33 +0100

    creating library and friends databases

--- infrastructure/init_db.sh
+++ infrastructure/init_db.sh
@@ -3,7 +3,7 @@ set -e
 
 echo "Creating additional databases: 'library' and 'friends'..."
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d postgres <<-EOSQL
     CREATE DATABASE library;
     CREATE DATABASE friends;
     GRANT ALL PRIVILEGES ON DATABASE library TO $POSTGRES_USER;

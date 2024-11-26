95d06fa4b5fa2a81f14f42fe201a7e2873b80e16
Leonid  Romaniukha <leonid.romaniukha@student.kdg.be>
2024-11-26 09:26:34 +0100

    added friends in data.sql

--- application/src/main/resources/data.sql
+++ application/src/main/resources/data.sql
@@ -8,3 +8,10 @@ VALUES
     ('95592121-d766-4715-a205-fbd13323ccc3', 'Victor', 'John', 'Doe', 'MALE'),
     ('95593601-d766-4715-a205-fbd13323ccc3', 'Alice', 'John', 'Doe', 'MALE'),
     ('95542621-d766-4715-a205-fbd13323ccc3', 'Luffy', 'John', 'Doe', 'MALE');
+
+insert into friends(player_id, friend_id)
+values
+    ('95592601-d766-4715-a205-fbd13323ccc3', '95592621-d766-4715-a205-fbd13323ccc3'),
+    ('95592601-d766-4715-a205-fbd13323ccc3', '95592701-d766-4715-a205-fbd13323ccc3'),
+    ('95592601-d766-4715-a205-fbd13323ccc3', '95592821-d766-4715-a205-fbd13323ccc3'),
+    ('95592601-d766-4715-a205-fbd13323ccc3', '95592301-d766-4715-a205-fbd13323ccc3');

5aa7c3cb61fdd394c981353887ce6dfff70ed182
Ali Al-Zamili <ali.alzamili@student.kdg.be>
2024-11-27 10:01:57 +0100

    fixing the merge for the lobby

--- lobby/src/main/java/be/kdg/prog6/lobby/LobbyBondedContextApplication.java
+++ lobby/src/main/java/be/kdg/prog6/lobby/LobbyBondedContextApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LobbyApplication {
+public class LobbyBondedContextApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(LobbyApplication.class, args);
+        SpringApplication.run(LobbyBondedContextApplication.class, args);
     }
 
 }
--- lobby/src/main/java/be/kdg/prog6/lobby/adapters/out/Repository/LobbyRepository.java
+++ lobby/src/main/java/be/kdg/prog6/lobby/adapters/out/Repository/LobbyRepository.java
@@ -3,10 +3,12 @@ package be.kdg.prog6.lobby.adapters.out.Repository;
 
 import be.kdg.prog6.lobby.adapters.out.entity.LobbyEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.UUID;
 
 public interface LobbyRepository extends JpaRepository<LobbyEntity, UUID> {
 
-    LobbyEntity loadByLobbyId(UUID id);
+    @Query("SELECT l FROM LobbyEntity l WHERE l.lobbyId = :lobbyId")
+    LobbyEntity loadByLobbyId(UUID lobbyId);
 }

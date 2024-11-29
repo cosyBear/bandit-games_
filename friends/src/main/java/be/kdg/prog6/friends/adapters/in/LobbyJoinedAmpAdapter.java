fb941a99afcf9c92c815ae172a4d3e72064dea50
Ali Al-Zamili <ali.alzamili@student.kdg.be>
2024-11-29 16:15:10 +0100

    moving file into the correct place

---
+++ friends/src/main/java/be/kdg/prog6/friends/adapters/in/LobbyJoinedAmpAdapter.java
@@ -0,0 +1,34 @@
+package be.kdg.prog6.friends.adapters.in;
+
+
+import be.kdg.prog6.common.events.LobbyJoinedEvent;
+import be.kdg.prog6.friends.domain.LobbyId;
+import be.kdg.prog6.friends.port.in.UpdateLobbyCommand;
+import be.kdg.prog6.friends.port.in.UpdateLobbyStatusUseCase;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class LobbyJoinedAmpAdapter
+{
+
+
+    private final UpdateLobbyStatusUseCase updateLobbyStatusUseCase;
+
+    @RabbitListener(queues = "JOINLobbyQueue")
+    public void joinLobby(LobbyJoinedEvent event){
+
+        UpdateLobbyCommand command  = new UpdateLobbyCommand(new LobbyId(event.lobbyId()), event.Status());
+
+        log.info("updating the lobby");
+
+        updateLobbyStatusUseCase.updateLobbyStatus(command);
+    }
+
+
+
+}

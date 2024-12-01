763cfb72175ef88d9a965d688ab33f7c5bff82a3
Ali Al-Zamili <ali.alzamili@student.kdg.be>
2024-12-01 06:59:29 +0100

    fixing script that creates the database

--- infrastructure/init_db.sh
+++ infrastructure/init_db.sh
@@ -1,20 +1,30 @@
-#!/bin/bash
-set -e
+#!/usr/bin/env bash
+
+set -e  # Exit immediately on error
 
 echo "Initializing databases: library and friends..."
 
-# Create 'warehouse_db'
+# Create 'library' database
 mysql -u root -p"${MYSQL_ROOT_PASSWORD}" <<EOF
 CREATE DATABASE IF NOT EXISTS library;
 GRANT ALL PRIVILEGES ON library.* TO '${MYSQL_USER}'@'%';
 FLUSH PRIVILEGES;
 EOF
 
-# Create 'water_db'
+# Create 'friends' database
 mysql -u root -p"${MYSQL_ROOT_PASSWORD}" <<EOF
 CREATE DATABASE IF NOT EXISTS friends;
 GRANT ALL PRIVILEGES ON friends.* TO '${MYSQL_USER}'@'%';
 FLUSH PRIVILEGES;
 EOF
+
+
+# Create 'friends' database
+mysql -u root -p"${MYSQL_ROOT_PASSWORD}" <<EOF
+CREATE DATABASE IF NOT EXISTS lobby;
+GRANT ALL PRIVILEGES ON lobby.* TO '${MYSQL_USER}'@'%';
+FLUSH PRIVILEGES;
+EOF
+
 
 echo "Databases initialized successfully!"

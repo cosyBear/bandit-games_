d9d63a8c84d4487d385add92a1898bdeb9de3aee
tea recica <tea.recica@student.kdg.be>
2025-01-03 14:39:01 +0100

    improved export and Predictions

--- statistics/src/main/java/be/kdg/prog6/statistics/ports/out/ExportServicePort.java
+++ statistics/src/main/java/be/kdg/prog6/statistics/ports/out/ExportServicePort.java
@@ -2,6 +2,8 @@ package be.kdg.prog6.statistics.ports.out;
 
 import be.kdg.prog6.statistics.domain.ExportData;
 
+import java.util.UUID;
+
 public interface ExportServicePort {
-    ExportData exportPlayerStatistics();
+    ExportData exportPlayerStatistics(UUID userId);
 }
